c82c6c5c6814a6a9c927e8f4d7067d0091586d26
Enosbez <enosbezerra3@gmail.com>
2024-10-10 15:47:23 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,22 @@
+let dados = [
+    {
+        titulo:"Gabigol",
+        descricao:"Gabigol é considerado ídolo no Flamengo",
+        link:"https://pt.wikipedia.org/wiki/Gabigol",
+        tags:"gabigol babriel barbosa flamengo"
+    },
+    {
+        titulo:"Zico",
+        descricao:"Zico é o maior ídolo do Flamengo",
+        link:"https://pt.wikipedia.org/wiki/Zico",
+        tags:"zico galinho"
+    },
+
+    {
+        titulo:"Neymar",
+        descricao:"Neymar é o maior artilheiro da seleção",
+        link:"https://pt.wikipedia.org/wiki/Neymar",
+        tags:"queda dribles brasileiro dança"
+    }
+
+];
